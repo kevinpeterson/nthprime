66c3d905e877c7f61180f17ae201257d02b42f8f
Kevin Peterson <kevin.peterson@mayo.edu>
2012-11-08 08:42:56 -0600

    Updated comments.

--- nthprime.c
+++ nthprime.c
@@ -4,14 +4,18 @@
 */
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int get_nth_prime(int nth_prime) {
+int divide_check(int nth_prime) {
     int primes = 0;
     int current_prime;
 
     int i;
-    for(i=2;primes<nth_prime;i++){
+    for(i=1;primes<nth_prime;i+=2){
         char prime = 1;
+        
+        //I've found here that j*j<=i is about 4x faster
+        //than j<=sqrt(i). YMMV.
         int j;
         for(j=2;j*j<=i;j++){
             if(i % j == 0 && i != j){
@@ -28,11 +32,51 @@ int get_nth_prime(int nth_prime) {
     return current_prime;
 }
 
+int sieve(int nth_prime) {
+    //approximate the prime number given n. This is guarenteed
+    //to be > the actual nth prime. This is used to determine
+    //how big our sieve must be.
+    int nth_prime_approx_max = ( log(nth_prime) + log(log(nth_prime)) ) * nth_prime;
+
+    int i,j;
+    char *sieve = calloc(nth_prime_approx_max, 1);
+    for (i=2;i*i<=nth_prime_approx_max; i++) {
+        if (!sieve[i]) {
+            for(j=i+i;j<nth_prime_approx_max;j+=i) { 
+                sieve[j] = 1; 
+            }
+        }
+    }
+
+    int prime_to_return;
+
+    int primes = 0;
+    for (i=2;i<nth_prime_approx_max && !prime_to_return; i++) {
+        if (!sieve[i]) { 
+            primes++;
+            if(primes == nth_prime){
+               prime_to_return = i;
+            }
+        }
+    }
+
+    free(sieve);
+    return prime_to_return;
+}
+
 int main(int argc, char* argv[]) {
     int nth = atoi(argv[1]);
 
-    int prime = get_nth_prime(nth);
+    int prime;
 
+    //the sieve approx only works with n >= 6. Otherwise,
+    //revert to divide check
+    if(nth >= 6){
+        prime = sieve(nth);
+    } else {
+        prime = divide_check(nth);
+    }
+    
     printf("%i\n", prime);
 
     return 0;
